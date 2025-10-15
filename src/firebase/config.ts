438db539a5f1d5a39fac1ea6491119afed84eb39
wagner sousa <wagner.projecta@gmail.com>
2025-10-15 16:35:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2293323909-9e575",
+  "appId": "1:1099442742027:web:67951df3865a7fb857780e",
+  "apiKey": "AIzaSyCdXIKYNJ64Ne_kKH6mAPok85MofW70kP0",
+  "authDomain": "studio-2293323909-9e575.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1099442742027"
+};
